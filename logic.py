9865fcae846ce1cc49607a7a8307a971dd05f077
vtopiha66-sudo <vtopiha66@gmail.com>
2026-04-17 07:18:36 +0200

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,1 +1,18 @@
+from db import DB
+import aiosqlite
 
+async def get_stock():
+    async with aiosqlite.connect(DB) as db:
+        cursor = await db.execute("SELECT name, stock FROM products")
+        return await cursor.fetchall()
+
+async def add_sale():
+    # пока просто заглушка
+    return "OK"
+
+async def check_deficit():
+    async with aiosqlite.connect(DB) as db:
+        cursor = await db.execute(
+            "SELECT name FROM products WHERE stock <= min"
+        )
+        return await cursor.fetchall()
