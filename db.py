1c223866151b00472be0fa2a68999f446f6cde9d
vtopiha66-sudo <vtopiha66@gmail.com>
2026-04-17 07:18:18 +0200

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,28 @@
+import aiosqlite
 
+DB = "db.sqlite"
+
+async def init_db():
+    async with aiosqlite.connect(DB) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            lang TEXT DEFAULT 'ua'
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS products (
+            name TEXT PRIMARY KEY,
+            stock REAL,
+            min REAL
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS dishes (
+            name TEXT PRIMARY KEY
+        )
+        """)
+
+        await db.commit()
