eb462edc82057c33658c653f88578b9b86b43a16
vtopiha66-sudo <vtopiha66@gmail.com>
2026-04-17 08:57:43 +0200

    Update logic.py

--- logic.py
+++ logic.py
@@ -6,9 +6,6 @@ async def get_stock():
         cursor = await db.execute("SELECT name, stock FROM products")
         return await cursor.fetchall()
 
-async def add_sale():
-    # пока просто заглушка
-    return "OK"
 
 async def check_deficit():
     async with aiosqlite.connect(DB) as db:
